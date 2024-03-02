6ad3c92dacfb3700ea1a200af8d475c345e9a685
shamirHere <aslanshamir@gmail.com>
2024-03-03 00:42:34 +0530

    add register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -47,7 +47,7 @@ const userSchema = new mongoose.Schema(
 // this thing is checking if the user have modified the password and then bcrypting it
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 userSchema.methods.isPasswordCorrect = async function (password) {
@@ -81,4 +81,4 @@ userSchema.methods.generateRefreshToken = async () => {
 };
 
 const User = mongoose.model("User", userSchema);
-export { userSchema };
+export { User };
